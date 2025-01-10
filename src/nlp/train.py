61ae01ff2b887c48608920b39b012f47df94231b
root <root@LAPTOP-3P8JESL3.>
2025-01-10 12:48:09 +0100

    Train args

--- src/nlp/train.py
+++ src/nlp/train.py
@@ -1,5 +1,8 @@
 from pytorch_lightning import Trainer, ModelCheckpoint, EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
 from src.nlp.model import nlp_model
+from src.nlp.data import EmbeddingDataset
+from loguru import logger
 
 def define_callbacks():
         checkpoint_callback = ModelCheckpoint(
@@ -10,20 +13,32 @@ def define_callbacks():
         )
         return [checkpoint_callback, early_stopping_callback]
 
-def train_nlp_model(train_dataloader, val_dataloader, test_dataloader):
+def train_nlp_model(embedding_save_path: str = "data/processed/embeddings.pt", model_name: str = "distilbert-base-uncased", size: int = 3000, data_seed: int = 42):
     """Train a nlp shallow model to classify text embedded with BERT into two categories.
     
-    Arguemnts:
-        None
+    Arguments:
+        embedding_save_path {str} -- Path to the embeddings file
+        model_name {str} -- Name of the BERT model to use as preprocessing
     Returns:
         None
     """
+    logger.info("Initializing the dataset...")
 
+    dataset = EmbeddingDataset(
+        model_name=model_name,
+        embedding_save_path=embedding_save_path,
+        size=size,
+        seed=data_seed,
+    )
+
+
+    logger.info("Initializing the model...")
     model = nlp_model()
 
+    logger.info("Training the model...")
     trainer = Trainer(callbacks=[define_callbacks()], 
                       max_epochs=10, 
-                      logger=pl.loggers.WandbLogger(project="dtu_mlops"))
+                      logger=WandbLogger(project="dtu_mlops"))
 
     trainer.fit(model, train_dataloader, val_dataloader)
     trainer.test(model, test_dataloader)
