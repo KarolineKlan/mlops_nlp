8659231bc6c8f443c60df7c0f5155407553966ee
kridk999 <17kkjaer@gmail.com>
2025-01-24 11:12:00 +0100

    added inference api at api.py

--- src/nlp/api.py
+++ src/nlp/api.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 import torch
 from fastapi import FastAPI, File, UploadFile
+from loguru import logger
 from transformers import AutoModel, AutoTokenizer
 
 from nlp.model import nlpModel
@@ -15,8 +16,9 @@ async def lifespan(app: FastAPI):
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
     embedding = AutoModel.from_pretrained("distilbert-base-uncased")
     classifier = nlpModel.load_from_checkpoint("models/SimpleModel.ckpt")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     embedding.to(device)
+    classifier.to(device)
     # gen_kwargs = {"max_length": 16, "num_beams": 8, "num_return_sequences": 1}
 
     yield
@@ -36,9 +38,15 @@ def read_root():
 @app.post("/review/")
 async def inference(input_string: str = "it was a bad movie"):
     """Infer semantics given a IMDB review."""
+    """Infer semantics given an IMDB review."""
     inputs = tokenizer(input_string, return_tensors="pt", padding=True, truncation=True)
     inputs.to(device)
     with torch.no_grad():
-        bert_embedding = embedding(**inputs)
+        bert_output = embedding(**inputs)
+        bert_embedding = bert_output.last_hidden_state[:, 0, :]  # Get the last hidden state
+        # Pass the embeddings to the classifier
+
         output = classifier(bert_embedding)
-    return output
+
+        label = ["positive" if output > 0.5 else "negative"]
+    return label[0], output.item()
